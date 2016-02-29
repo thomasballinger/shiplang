fd17ca347151d0b6dc9ccbbcf54e33d78463b50e
Thomas Ballinger <thomasballinger@gmail.com>
2016-02-29 15:12:16 -0500

    add lodash as dependency

--- scenarios.ts
+++ scenarios.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash';
+
 import { System, makeShip, makeBoid, makePlanet, makeComponent } from './system';
 import { SLgetScripts } from './scriptenv';
 import { WorldBuilder, ShipSpec } from './interfaces';
@@ -18,32 +20,29 @@ export var gunner = function():any{
     <a href="/#space" onclick="Player.fromStorage().set('location', 'Sol').set('script', window.normalScript).go();">quit this job, it sucks</a>`);
 
     var NUMCIVILIANS = 30
-    var civilianArgs = <[ShipSpec, number, number, number][]>[];
-    for (var i=0; i<NUMCIVILIANS; i++){
-      civilianArgs.push([
-         [ships.Boid, ships.Shuttle, ships.Triangle][Math.floor(Math.random() * 3)],
-         Math.random()*2000 - 1000,
-         Math.random()*2000 - 1000,
-         Math.random() * 360,
-         builtinScripts.citizenScript,
-      ]);
-    }
+    var civilianArgs = _.range(NUMCIVILIANS).map(function(){
+        return [
+            [ships.Boid, ships.Shuttle, ships.Triangle][Math.floor(Math.random() * 3)],
+            Math.random()*2000 - 1000,
+            Math.random()*2000 - 1000,
+            Math.random() * 360,
+            builtinScripts.citizenScript,
+        ];
+    });
 
     var reset = <WorldBuilder>function reset(script: any): System {
         var world = new System();
         var p = Player.fromStorage().spaceLocation;
         var ship = makeComponent(ships.Gunship, p[0], p[1], 270, script);
         ship.imtheplayer = true;
-        world.addBackgroundEntity(makePlanet(400, 300, 40, '#ab43af'));
-        world.addBackgroundEntity(makePlanet(-600, -800, 60, '#3bd951'));
-        world.addBackgroundEntity(makePlanet(-800, 600, 80, '#8b2141'));
+        world.addBackgroundEntity(makePlanet(1400, 1300, 40, '#ab43af'));
+        world.addBackgroundEntity(makePlanet(-1600, -1800, 60, '#3bd951'));
+        world.addBackgroundEntity(makePlanet(-1800, 1600, 80, '#8b2141'));
         world.addEntity(makeShip(ships.Astroid, -300, 350, 270, builtinScripts.wander));
         world.addEntity(makeShip(ships.Astroid,  300, 350, 270, builtinScripts.wander));
         world.addEntity(makeShip(ships.Astroid, -300, -350, 270, builtinScripts.wander));
 
-        civilianArgs.map(function(x){
-            world.addEntity(makeShip.apply(null, x))
-        })
+        civilianArgs.map(function(x){ world.addEntity(makeShip.apply(null, x)) })
         world.addEntity(makeShip(ships.Holder, 0, 200, 270, builtinScripts.holderScript));
         world.addEntity(ship); // adding the ship last means it goes in front
 
