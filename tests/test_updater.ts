700d0f52c1b88e1e858df9bf5f71993412a343cf
Thomas Ballinger <thomasballinger@gmail.com>
2016-03-22 01:20:02 -0400

    start testing updater

---
+++ tests/test_updater.ts
@@ -0,0 +1,47 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+import { assert } from 'chai';
+
+import { Updater } from '../updater';
+import { Engine } from '../engine';
+import { System, universe, createObjects } from '../universe';
+import { Selection } from '../interfaces';
+import { Profile } from '../profile';
+import { loadData } from '../dataload';
+
+
+var fixtures = createObjects(loadData(`
+system Sys
+	pos 0 0
+	government Trader
+`.replace(/    /g, '\t')));
+
+
+describe('Updater', () => {
+    it("Can be constructed too much mocking", () => {
+
+        // TODO allow an unbound InputHandler object
+        var div = document.createElement("div");
+        div.id = 'asdf'
+        document.body.appendChild(div);
+
+        var codeToLoad = '';
+        var updater = new Updater((msg: string)=>{},
+                                  ()=>{},
+                                  (msg: string)=>{},
+                                  ()=>{ return codeToLoad; },
+                                  'asdf',
+                                  new Engine(fixtures.systems['sys'], Profile.newProfile()),
+                                  'javascript',
+                                  ()=>{},
+                                  (id: string, selections: Selection[])=>{},
+                                  true,
+                                  (e: any)=>{});
+
+    });
+    describe('#resetFromSave', () => {
+        it("Can restore a saved state", () => {
+        });
+        it("Can reset to original state", () => {
+        });
+    });
+});
