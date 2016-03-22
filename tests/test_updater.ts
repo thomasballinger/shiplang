d3b6b0d69084292c4da41926974b2f85b8a8b115
Thomas Ballinger <thomasballinger@gmail.com>
2016-03-22 18:42:56 -0400

    refactor tests

--- tests/test_updater.ts
+++ tests/test_updater.ts
@@ -24,67 +24,50 @@ system Sys
 `.replace(/    /g, '\t')));
 console.log('fixtures:', fixtures);
 
+/** Returns updater and code update function */
+function buildUpdater(): [Updater, (code: string)=>void]{
+    var codeToLoad = '';
+    var updater = new Updater(new Engine(fixtures.systems['Sys'],
+                                         Profile.newProfile()),
+                              ()=>{ return codeToLoad; },
+                              true);
+    updater.userFunctionBodies = new FakeUserFunctionBodies();
+    return [updater, function updateCode(code: string){
+        codeToLoad = code;
+        updater.notifyOfCodeChange();
+    }];
+
+}
+
 
 describe('Updater', () => {
     it("Can be constructed too much mocking", () => {
-        var codeToLoad = '';
-        var updater = new Updater(new Engine(fixtures.systems['Sys'],
-                                             Profile.newProfile()),
-                                  ()=>{ return codeToLoad; },
-                                  true);
-
+        var [updater, _] = buildUpdater();
     });
     describe('detects when reset or restart needed', () => {
-        it("doesn't reset when an unrun function changes", () => {
-            var codeToLoad = '';
-            var updater = new Updater(new Engine(fixtures.systems['Sys'],
-                                                 Profile.newProfile()),
-                                      ()=>{ return codeToLoad; },
-                                      true);
-            updater.userFunctionBodies = new FakeUserFunctionBodies();
-            codeToLoad = 'function foo(){ return 1; };';
-            updater.notifyOfCodeChange();
+        it("Doesn't reset when an unrun function changes", () => {
+            var [updater, updateCode] = buildUpdater();
+            updateCode('function foo(){ return 1; };');
             updater.tick(0.01)
-
-            updater.tick(0.1);
-            codeToLoad = 'function foo(){ return 2; };';
-            updater.notifyOfCodeChange();
+            updateCode('function foo(){ return 2; };');
             updater.tick(0.03);
-            assert.equal(updater.world.gameTime, 0.14);
+            assert.equal(updater.world.gameTime, 0.04);
         });
         it("Resets from beginning when a new function is added", () => {
-            var codeToLoad = '';
-            var updater = new Updater(new Engine(fixtures.systems['Sys'],
-                                                 Profile.newProfile()),
-                                      ()=>{ return codeToLoad; },
-                                      true);
+            var [updater, updateCode] = buildUpdater();
             updater.userFunctionBodies = new FakeUserFunctionBodies();
-            codeToLoad = 'function foo(){ return 1; };';
-            updater.notifyOfCodeChange();
+            updateCode('function foo(){ return 1; };');
             updater.tick(0.01)
-
-            updater.tick(0.1);
-            codeToLoad = 'function foo(){ return 2; }; function bar(){ return 3; }';
-            updater.notifyOfCodeChange();
+            updateCode('function foo(){ return 2; }; function bar(){ return 3; }');
             updater.tick(0.03);
             assert.equal(updater.world.gameTime, 0.03);
         })
         it("Resets to previous state when a called function changes", () => {
-            var codeToLoad = '';
-            var updater = new Updater(new Engine(fixtures.systems['Sys'],
-                                                 Profile.newProfile()),
-                                      ()=>{ return codeToLoad; },
-                                      true);
-            var bodies = new FakeUserFunctionBodies();
-            updater.userFunctionBodies = bodies;
-            codeToLoad = 'function foo(){ return 1; };';
-            updater.notifyOfCodeChange();
-
-            bodies.pretendFunctionBodyWasAccessed('foo');
+            var [updater, updateCode] = buildUpdater();
+            updateCode('function foo(){ return 1; };');
+            (<FakeUserFunctionBodies>updater.userFunctionBodies).pretendFunctionBodyWasAccessed('foo');
             updater.tick(0.01)
-
-            codeToLoad = 'function foo(){ return 2; }';
-            updater.notifyOfCodeChange();
+            updateCode('function foo(){ return 2; }');
             updater.tick(0.03);
             assert.equal(updater.world.gameTime, 0.03);
         });
